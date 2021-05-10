7ac57ddb7b77482b338e14ee1f1edfff0b3126f3
Tanay Raj <tanay.raj76@gmail.com>
2021-05-10 20:03:01 +0530

    subset sum and Equal partition

---
+++ Dynamic Programming/equalPartition.cpp
@@ -0,0 +1,43 @@
+class Solution{
+public:
+    bool equalPartition(int N, int arr[])
+    {
+        // code here
+        
+        int dpSum;
+        int sum = 0;
+        for(int i=0;i<N;i++)
+        {
+            sum+=arr[i];
+        }
+        if(sum%2!=0)
+        return false;
+
+          dpSum = sum/2;
+          bool subset[N+1][dpSum+1];
+          for (int i = 0; i <= N; i++)
+             subset[i][0] = true;
+        
+          for (int i = 1; i <= dpSum; i++)
+              subset[0][i] = false;
+       
+          for (int i = 1; i <= N; i++) 
+          {
+            for (int j = 1; j <= dpSum; j++) 
+            {
+                if (j < arr[i - 1])
+                    subset[i][j] = subset[i - 1][j];
+                if (j >= arr[i - 1])
+                    subset[i][j] = subset[i - 1][j] || subset[i - 1][j - arr[i - 1]];
+            }
+          }
+ 
+    
+            
+        
+        
+         return subset[N][dpSum];
+          
+        
+    }
+};
---
+++ Dynamic Programming/subSetSum.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+ 
+
+bool isSubsetSum(int set[], int n, int sum)
+{
+    bool subset[n + 1][sum + 1];
+ 
+    // If sum is 0, then answer is true
+    for (int i = 0; i <= n; i++)
+        subset[i][0] = true;
+
+    // if array is empty then answer is false
+    for (int i = 1; i <= sum; i++)
+        subset[0][i] = false;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= sum; j++) {
+            if (j < set[i - 1])
+                subset[i][j] = subset[i - 1][j];
+            if (j >= set[i - 1])
+                subset[i][j] = subset[i - 1][j]
+                               || subset[i - 1][j - set[i - 1]];
+        }
+    }
+ 
+    return subset[n][sum];
+}
+ 
+// Driver code
+int main()
+{
+    int set[] = { 3, 34, 4, 12, 5, 2 };
+    int sum = 9;
+    int n = sizeof(set) / sizeof(set[0]);
+    if (isSubsetSum(set, n, sum) == true)
+        printf("Found a subset with given sum");
+    else
+        printf("No subset with given sum");
+    return 0;
+}
