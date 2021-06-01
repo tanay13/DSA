850ec2eada0c962df7b90ae11ee2042162d1ce9c
Tanay Raj <tanay.raj76@gmail.com>
2021-06-02 00:17:13 +0530

    jump game 1

---
+++ LeetCode/jumpGame.cpp
@@ -0,0 +1,11 @@
+    bool canJump(vector<int>& nums) {
+    int i = 0;
+    for (int reach = 0; i < nums.size() && i <= reach; ++i)
+        reach = max(i + nums[i], reach);
+    return i == nums.size();
+        
+    }
+
+    // if reach gets equal to or less than i then it means the jump is blocked 
+    //and further indexes cannot be reached so we end the loop there and check the equality 
+    //of reach and size of vector
